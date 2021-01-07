6f29762678392e111c65ca494fedf08b238826d0
Aleksey Klokov <lex-oz@yandex.ru>
2021-01-07 07:23:50 +0300

    fix error

--- src/test/java/ru/netology/domain/BookTest.java
+++ src/test/java/ru/netology/domain/BookTest.java
@@ -21,15 +21,10 @@ class BookTest {
   }
 
   @Test
-  public void shouldNotCastToDifferentClass() {
-    Product product = new Book();
-    TShirt shirt = (TShirt) product;
-  }
-
-  @Test
   public void shouldUseOverridedMethod() {
     Product product = new Book();
     // Вопрос к аудитории: чей метод вызовется?
     product.toString();
   }
+
 }
